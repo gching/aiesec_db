86c0e18a366d5ce8f65db48b92c870cc7d0b465d
Gavin Ching <gavinchingy@gmail.com>
2013-05-16 16:24:01 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AiesecDb::Application.config.secret_token = '74768a8eb6655e95d63808db39ab0da1c33b6437e8d7d16efc96c11b0443ab3b0928cfab1c40d3e81a4888baace04206aa750929613153913480b78c0bc130ef'
